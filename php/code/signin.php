6eb2ba959ebd4ff9277be9fe07db2a3474028e69
llevasse <elise.levasse@gmail.com>
2025-12-12 23:23:07 +0100

    [php|db] added check for missing var in sign in

--- php/code/signin.php
+++ php/code/signin.php
@@ -1,4 +1,11 @@
 <?php
+  ob_start();
+  function error_quit($error) {
+    ob_clean();
+    http_response_code(400);
+    die($error);
+  }
+
   $db_password = ($_ENV['DB_PASSWORD']);
   $db_user = ($_ENV['DB_USER']);
   $db_host = ($_ENV['DB_HOST']);
@@ -6,7 +13,7 @@
   $conn = mysqli_connect($db_host, $db_user, $db_password);
   
   if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_quit("Connection failed: " . $conn->connect_error);
   }  
   
   $input = $_POST;
@@ -14,21 +21,31 @@
     $json = file_get_contents('php://input');
     $input = json_decode($json, true);
     if (json_last_error() !== JSON_ERROR_NONE){
-      die('Error parsing json : '. json_last_error_msg());
+      error_quit('Error parsing json : '. json_last_error_msg());
     }
   }
   
   $sql = "INSERT INTO camagru.users (email, username, password_hash) VALUES (?, ?, ?)";
   $stmt = $conn->prepare($sql);
   $email = $input['email'];
+  if (!$email){
+    error_quit("No email provided");
+  }
   $username = $input['username'];
+  if (!$email){
+    error_quit("No email provided");
+  }
+  $password = $input['password'];
+  if (!$password){
+    error_quit("No password provided");
+  }
   $password = password_hash($input['password'], PASSWORD_DEFAULT);
   
   if ($stmt->bind_param("sss", $email, $username, $password) === false) {
-    die("SQL params binding failed: " . $stmt->error);
+    error_quit("SQL params binding failed: " . $stmt->error);
   }
   if ($stmt->execute() === false) {
-    die("SQL execute failed: " . $stmt->error);
+    error_quit("SQL execute failed: " . $stmt->error);
   }
   echo "New user created successfully";
   
