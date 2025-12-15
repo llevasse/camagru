9831820fa300e9636d6ae8483f236205bca81198
llevasse <elise.levasse@gmail.com>
2025-12-15 16:09:36 +0100

    [front] can upload local images

--- front/html/js/pages/editing.js
+++ front/html/js/pages/editing.js
@@ -4,9 +4,11 @@ class EditingPage{
   <div id="editing-container">
     <div id="editing-main-div">
       <video id="editing-video"></video>
+      <img id="editing-video-img"></img>
       <div id="editing-video-buttons-container">
         <button id="editing-permissions-button">Allow webcam capture</button>  
         <button id="editing-capture">Capture photo</button>
+        <input id="editing-upload-button" type="file" accept="image/*">
       </div>
     </div>
     <div id="editing-side-div">
@@ -25,22 +27,67 @@ class EditingPage{
   streaming = false;
   
   video;
+  editingImg;
   canvas;
   photo;
   captureButton;
   permsisionButton;
+  uploadButton;
   saveButton;
+  
+  focusSource;
+  
+  removeStream(){
+    if (this.streaming){  // cancel stream
+      this.video.srcObject.getTracks().forEach(track=>track.stop());
+      this.streaming = false;
+      this.permsisionButton.innerHTML = "Allow webcam capture";
+
+      
+      return 1;
+    }
+    return 0
+  }
+  
+  setVideoAsSrc(){
+    if (this.editingImg.classList.contains("active")){
+      this.editingImg.classList.remove("active");
+    }
+    if (!this.video.classList.contains("active")){
+      this.video.classList.add("active");
+    }
+    this.focusSource = this.video;
+  }
+  setImgAsSrc(){
+    if (this.video.classList.contains("active")){
+      this.video.classList.remove("active");
+    }
+    if (!this.editingImg.classList.contains("active")){
+      this.editingImg.classList.add("active");
+    }
+    this.focusSource = this.editingImg;
+  }
   
   constructor(){
     history.replaceState("","","https://localhost:833/editing")
     document.getElementById("container").innerHTML = this.body;
     
     this.video = document.getElementById("editing-video");
+    this.editingImg = document.getElementById("editing-video-img");
     this.canvas = document.getElementById("editing-canvas");
     this.photo = document.getElementById("editing-photo");
     this.captureButton = document.getElementById("editing-capture");
     this.permsisionButton = document.getElementById("editing-permissions-button");
     this.saveButton = document.getElementById("save-photo-button");
+    this.uploadButton = document.getElementById("editing-upload-button");
+    
+    this.uploadButton.addEventListener("change", (ev)=>{
+      if (this.uploadButton.files.length !== 0){
+        this.removeStream();
+        this.setImgAsSrc();
+        this.editingImg.src = URL.createObjectURL(this.uploadButton.files[0]);
+      }
+    })
     
     this.video.addEventListener("canplay", ()=>{
       if (!this.streaming) {
@@ -55,15 +102,14 @@ class EditingPage{
     
     this.captureButton.addEventListener("click", (ev)=>{
       const context = this.canvas.getContext("2d");
-      const videoSize = this.video.getBoundingClientRect();
-      // const width = this.video.videoWidth;
-      // const height = this.video.videoHeight;
-      const width = videoSize.width;
-      const height = videoSize.height;
-      if (width && height) {
-        this.canvas.width = width;
-        this.canvas.height = height;
-        context.drawImage(this.video, 0, 0, width, height);
+      const captureSize = this.focusSource.getBoundingClientRect();
+      const width = captureSize.width;
+      const height = captureSize.height;
+      const s = width > height ? height : width;
+      if (s) {
+        this.canvas.width = s;
+        this.canvas.height = s;
+        context.drawImage(this.focusSource, 0, 0, s, s);
   
         const data = this.canvas.toDataURL("image/png");
         this.photo.setAttribute("src", data);
@@ -77,21 +123,24 @@ class EditingPage{
       }
       ev.preventDefault();
     });
-    
+  
+       
     this.permsisionButton.addEventListener("click", ()=>{
-      if (this.streaming){  // cancel stream
-        this.video.srcObject.getTracks().forEach(track=>track.stop());
-        this.streaming = false;
-        this.permsisionButton.innerHTML = "Allow webcam capture";
-        return;
-      }
+      if(this.removeStream()) {
+        if (this.editingImg.files.length !== 0){
+          this.setImgAsSrc();
+        }
+        this.video.classList.remove("active");
+      };
       
       navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then((stream) => {
         document.getElementById("editing-video").srcObject = stream;
         document.getElementById("editing-video").play();
+        
         this.permsisionButton.innerHTML = "Remove webcam capture";
+        this.setVideoAsSrc();
       })
       .catch((err) => {
         console.error(`An error occurred: ${err}`);
