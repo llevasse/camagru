c27b17690ba9d4c5f2be946d6c42914f04e1591c
llevasse <elise.levasse@gmail.com>
2026-08-04 14:50:57 +0200

    [fix] some stupif errors

--- php/code/pages/scripts/error_403.js
+++ php/code/pages/scripts/error_403.js
@@ -1,5 +1,5 @@
 body = `
-<link id="style" rel="stylesheet" href="/css/error-page.css">
+<link id="style" rel="stylesheet">
 <div id="error-page-container">
   <h2>You need to be logged in to access this page</h2>
 </div>`;
