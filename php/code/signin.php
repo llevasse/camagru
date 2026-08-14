555e9ff7ec5e0ec11a2584d87ad3ea64e49e688e
llevasse <elise.levasse@gmail.com>
2026-08-14 10:56:21 +0200

    [fix] resolves #45

--- php/code/signin.php
+++ php/code/signin.php
@@ -27,6 +27,18 @@
   
   $sql = "INSERT INTO camagru.users (email, username, password_hash) VALUES (?, ?, ?)";
   $stmt = $conn->prepare($sql);
+  
+	$username = $input['username'];
+  if (!$username){
+    quit(json_encode(array("message"=> "No username provided")), 400);
+  }
+  if (strlen($username) > 30){
+    quit(json_encode(array("message"=> "Username too long (max 30 character)")), 400);
+  }
+  if (preg_match("/\s/", $username)){
+    quit(json_encode(array("message"=> "Username can't contain any whitespace")), 400);
+  }
+
   $email = $input['email'];
   if (!$email){
     quit(json_encode(array("message"=> "No email provided")), 400);
@@ -37,20 +49,27 @@
   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     quit(json_encode(array("message"=> "Invalid email format")), 400);
   }
-  $username = $input['username'];
-  if (!$username){
-    quit(json_encode(array("message"=> "No username provided")), 400);
-  }
-  if (strlen($username) > 30){
-    quit(json_encode(array("message"=> "Username too long (max 30 character)")), 400);
-  }
-  if (containsAny($username, " \t\r\n")){
-    quit(json_encode(array("message"=> "Username can't contain any whitespace")), 400);
-  }
+
   $password = $input['password'];
   if (!$password){
     quit(json_encode(array("message"=> "No password provided")), 400);
   }
+	if (!preg_match("/[a-zA-Z]/", $password)){
+		quit(json_encode(array("message"=> "Password needs to contain at least one alphabetical latin characteur")), 400);
+	}
+	if (!preg_match("/[0-9]/", $password)){
+		quit(json_encode(array("message"=> "Password needs to contain at least one number")), 400);
+	}
+	if (!preg_match("/@|-|_|\+|=|<|>/", $password)){
+    quit(json_encode(array("message"=> "Password needs to contain one of these characters : @-_+=<>")), 400);
+  }
+  if (strlen($password) < 8){
+    quit(json_encode(array("message"=> "Password too short (min 8 character)")), 400);
+  }
+  if (strlen($password) > 100){
+    quit(json_encode(array("message"=> "Password too long (max 100 character)")), 400);
+  }
+  
   $password = password_hash($input['password'], PASSWORD_DEFAULT);
   
   if ($stmt->bind_param("sss", $email, $username, $password) === false) {
